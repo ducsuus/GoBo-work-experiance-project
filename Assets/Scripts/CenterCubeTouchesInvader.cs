f7fa11424591e226fedcfd9d97bf93234be92ef8
Evan Harwin <evanharwin@gmail.com>
2016-08-23 11:07:54 +0100

    Added Obelix, Gave it health and Invaders do damage.

---
+++ Assets/Scripts/CenterCubeTouchesInvader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterCubeTouchesInvader : MonoBehaviour {
+
+	public int CubeHealth = 5;
+
+	void OnCollisionEnter (Collision col) {
+
+		if(col.gameObject.tag == "Invader") {
+
+			//col.gameObject.GetComponent<ParticleSystem>().Emit(30);
+			Destroy(col.gameObject);
+
+			CubeHealth--;
+
+			if(CubeHealth <= 0) {
+
+				Destroy(gameObject);
+			
+			}
+
+
+		}
+	}
+}
---
+++ Assets/Scripts/InvaderExplodes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvaderExplodes : MonoBehaviour {
+
+	void Start () {
+
+	gameObject.GetComponent<ParticleSystem>().emissionRate = 0;
+	gameObject.GetComponent<ParticleSystem>().Emit(30);
+	Destroy(gameObject);
+
+	}
+
+//	public void Die () {
+//
+//			gameObject.GetComponent<ParticleSystem>().Emit(30);
+//			Destroy(gameObject);
+//		
+//		}
+	
+}
