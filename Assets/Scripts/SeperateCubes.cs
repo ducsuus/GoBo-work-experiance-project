82493f54137630b37a56d5def680dc3da687afc1
Evan Harwin <evanharwin@gmail.com>
2016-08-26 10:04:21 +0100

    Halfway though adding destructable things, not working yet.

---
+++ Assets/Scripts/SeperateCubes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeperateCubes : MonoBehaviour {
+
+	public float tension = 1; // Sets force between cubes
+
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log(this.transform.parent);
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+
+		this.transform.parent = null; // Breaks away from group
+
+		Destroy(this.gameObject, 1f);
+
+	}
+}
